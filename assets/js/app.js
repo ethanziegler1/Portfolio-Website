5d709f83d582359efc916c669ea100747922d5c4
Ethan Ziegler <ethan.ziegler1@gmail.com>
2024-07-17 16:45:18 -0400

    started to change style of website

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,30 @@
-// scroll to top functionality
-const scrollUp = document.querySelector("#scroll-to-top");
-scrollUp.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
-});
-const linkedIn = document.querySelector("#linked-in")
-linkedIn.addEventListener("click", () =>{
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth",
-      });
-})
+const sections = document.querySelectorAll('.section');
+const buttonContainer = document.querySelectorAll('.controls');
+const sectionBtn = document.querySelectorAll('.control');
+const allSections = document.querySelectorAll('.main-content');
+
+function PageTransitions(){
+    //Button click active class
+    for (let i = 0; i < sectionBtn.length; i++) {
+        sectionBtn[i].addEventListener('click', function(){
+            let currBtn = document.querySelectorAll('.active-btn');
+            currBtn[0].className = currBtn[0].className.replace('active-btn', '')
+            this.className += ' active-btn'
+        })
+    }
+
+    //Sections active class
+    allSections[0].addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if (id) {
+            //hide other sections
+            sections.forEach((section) =>{
+                section.classList.remove('active')
+            })
+            const element = document.getElementById(id);
+            element.classList.add('active')
+        }
+    })
+}
+
+PageTransitions();
